7e5957f3cb41fc9443a3957134dec01c96323ec0
Farhad Nuri <231006312@eastdelta.edu.bd>
2025-11-01 10:37:49 +0600

    Database connection

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,21 @@
+export async function signUp(req,res) {
+    try {
+
+    } catch (error) {
+
+    }
+}
+export async function logIn(req,res) {
+    try {
+
+    } catch (error) {
+
+    }
+}
+export async function logOut(req,res) {
+    try {
+
+    } catch (error) {
+
+    }
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,16 @@
 import express from "express"
+import authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv"
+import { connectDatabase } from "./config/database.config.js"
+
+dotenv.config()
+const PORT = process.env.PORT
 const app = express()
 
-app.listen(5001, () => {
-    console.log("Server running in 5001")
+app.use("/api/auth",authRoutes)
+
+connectDatabase().then(() => {
+   app.listen(PORT, () => {
+    console.log("✅ Server running on "+PORT)
+    }) 
 })
